93c71571df3779816370ffd348521fbca460909c
ABHISHEK JAISWAL <142292455+Abhishek-jaiswal03@users.noreply.github.com>
2024-08-24 22:57:34 +0530

    Add files via upload

---
+++ 1/sol.py
@@ -0,0 +1,7 @@
+(1) python --version
+(2) pvm is python virtual machine.when the source file is compiled it goes creates a python byte file called .pyc file.Each platform has diffrent pvm that make it platform independent.
+(3) just compile the source file.
+(4) python3 filename.py
+(5) true
+
+ 
