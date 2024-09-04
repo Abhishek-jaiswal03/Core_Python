121661f213950486c19aa004530f1983599349a0
ABHISHEK JAISWAL <142292455+Abhishek-jaiswal03@users.noreply.github.com>
2024-09-04 23:40:19 +0530

    Add files via upload

---
+++ 16/4.py
@@ -0,0 +1,7 @@
+n=int(input())
+count,i=0,1
+while(count!=n):
+    if(i%2!=0):
+        print(i,end=" ")
+        count+=1
+    i=i+1
---
+++ 16/5.py
@@ -0,0 +1,7 @@
+n=int(input())
+count=0
+i=2*n-1
+while(count<n):
+    print(i,end=' ')
+    count+=1
+    i-=2
